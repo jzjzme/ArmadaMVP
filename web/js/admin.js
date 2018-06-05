b36e4ea3158c664f49ae453f5d65a5d6e0db704d
Kirill Mangutov <kmangutov@gmail.com>
2018-06-05 19:30:38 +0800

    that didnt work so try using absolute url

--- web/js/admin.js
+++ web/js/admin.js
@@ -1,6 +1,6 @@
                                                   $("#order-product").click(function() {
   var _productId = $("#order-product-id").val();
-  axios.get("/order/" + _productId).then(function(resp) {
+  axios.get("http://206.189.90.181:3000/order/" + _productId).then(function(resp) {
     alert(JSON.stringify(resp));
   });
 
@@ -12,7 +12,7 @@ $("#checkpoint").click(function() {
   var _checkpointId = $("#checkpoint-id").val();
 
 
-  axios.get("/order/" + _productId + "/checkpoint/" + _checkpointId).then(function(resp) {
+  axios.get("http://206.189.90.181:3000/order/" + _productId + "/checkpoint/" + _checkpointId).then(function(resp) {
     alert(JSON.stringify(resp));
   });
 });
